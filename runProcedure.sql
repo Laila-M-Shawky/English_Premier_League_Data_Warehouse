6e70af9bca232273ab261421a1bda7b06d758047
Laila Shawky <145765904+Laila-M-Shawky@users.noreply.github.com>
2025-12-06 05:27:13 +0200

    Add files via upload

---
+++ runProcedure.sql
@@ -0,0 +1,5 @@
+EXEC dbo.usp_LoadEPL_21_22
+  @MatchCsvPath  = 'D:\1. GPA\Advanced Database\Assignment 1 2025\all_match_results.csv',
+  @PlayerCsvPath = 'D:\1. GPA\Advanced Database\Assignment 1 2025\all_players_stats.csv',
+  @PointsCsvPath = 'D:\1. GPA\Advanced Database\Assignment 1 2025\points_table.csv'
+GO
